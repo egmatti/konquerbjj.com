592e5a5f8b8251c07c9f0ca5e8f4e5d5e63aa327
Emily Mattison <egmatti@gmail.com>
2018-06-10 14:54:40 -0400

    Added accents to About section on home page, created Email Sign Up section, and tried to fix custom font issues

--- slate.config.js
+++ slate.config.js
@@ -3,18 +3,21 @@
 const path = require('path');
 
 const alias = {
-  jquery: path.resolve('./node_modules/jquery'),
-  'lodash-es': path.resolve('./node_modules/lodash-es'),
+	'lodash': path.resolve('./node_modules/lodash-es'),
+	'scripts': path.resolve('./src/assets/scripts'),
+	'styles': path.resolve('./src/assets/styles'),
+	'fonts': path.resolve('./src/assets/fonts'),
 };
 
 module.exports = {
-  slateCssVarLoader: {
-    cssVarLoaderLiquidPath: ['src/snippets/css-variables.liquid'],
-  },
-  slateTools: {
-    extends: {
-      dev: {resolve: {alias}},
-      prod: {resolve: {alias}},
-    },
-  },
+	slateTools: {
+		extends: {
+			dev: {
+				resolve: {alias}
+			},
+			prod: {
+				resolve: {alias}
+			},
+		},
+	}
 };
